83acaa1a13b6ee39e7916680b2b31525b33e296e
eros francesco reale <eros.francesco.francesco@gmail.com>
2026-02-18 15:27:51 +0000

    set up working be

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,6 @@
 use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
 
-mod timeout;
-mod notification;
-use notification::Notification;
-
 
 #[derive(Serialize, Deserialize)]
 struct Message {
@@ -23,18 +19,6 @@ async fn hello(name: web::Path<String>) -> HttpResponse {
     })
 }
 
-async fn notify() -> HttpResponse {
-    Notification::new()
-        .summary("Notificable Purr")
-        .body("This is a test notification from the backend!")
-        .show();
-        // .unwrap();
-
-    HttpResponse::Ok().json(Message {
-        message: "Notification sent!".to_string(),
-    })
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
@@ -48,8 +32,7 @@ async fn main() -> std::io::Result<()> {
             .service(
                 web::scope("/api")
                     .route("/health", web::get().to(health))
-                    .route("/hello/{name}", web::get().to(hello))
-                    .route("/notify", web::get().to(notify)),
+                    .route("/hello/{name}", web::get().to(hello)),
             )
     })
     .bind("127.0.0.1:3001")?
